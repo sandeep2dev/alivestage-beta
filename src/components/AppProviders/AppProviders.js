83476b2fd2f27eef5184c390f6ad1dcc740fe337
Sandeep Kumawat <codewick@outlook.com>
2026-08-01 22:14:28 +0530

    scroll on route change

--- src/components/AppProviders/AppProviders.js
+++ src/components/AppProviders/AppProviders.js
@@ -1,11 +1,13 @@
 'use client';
 
 import Navbar from '@/components/Navbar/Navbar';
+import ScrollToTop from '@/components/ScrollToTop/ScrollToTop';
 import { AuthProvider } from '@/contexts/AuthContext';
 
 export default function AppProviders({ children }) {
   return (
     <AuthProvider>
+      <ScrollToTop />
       <Navbar />
       <main>{children}</main>
     </AuthProvider>
